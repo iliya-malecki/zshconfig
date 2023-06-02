34e59d9335946aff72814799153e02e97058860c
Iliia <iliyamalecki@gmail.com>
2023-06-02 16:40:53 +0200

    first commit

---
+++ easy-install.sh
@@ -0,0 +1,11 @@
+plugins=${ZSH_CUSTOM:-~/.oh-my-zsh/custom/plugins}
+
+git clone https://github.com/esc/conda-zsh-completion $plugins/conda-zsh-completion
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $plugins/zsh-autosuggestions
+if [ ! -d "$plugins/tldr" ]; then
+    mkdir "$plugins/tldr"
+    curl -LJO https://github.com/dbrgn/tealdeer/releases/latest/download/completions_zsh -o "$plugins/tldr/_tldr.zsh"
+    echo -e 'fpath+="${0:h}"' > "$plugins/tldr/tldr.plugin.zsh"
+fi
+cp -b --suffix=".pre-autoconfig" .zshrc .p10k.zsh $HOME
---
+++ readme.md
@@ -0,0 +1,5 @@
+# My beloved zsh config
+one can run the full installation with `easy-install.sh` or just copy the configs with
+```bash
+cp -b --suffix=".pre-autoconfig" .zshrc .p10k.zsh $HOME
+```
