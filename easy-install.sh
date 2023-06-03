565a1015338addd3bd526072b5ae15a7873fbd3f
Iliia <iliyamalecki@gmail.com>
2023-06-03 14:37:21 +0200

    linux fs sucks

--- easy-install.sh
+++ easy-install.sh
@@ -7,9 +7,8 @@ yes | git clone https://github.com/esc/conda-zsh-completion $custom/plugins/cond
 yes | git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $custom/plugins/zsh-syntax-highlighting
 yes | git clone https://github.com/zsh-users/zsh-autosuggestions $custom/plugins/zsh-autosuggestions
 if ! command -v tldr >/dev/null; then
-    echo installing tldr
-    curl -LJ https://github.com/dbrgn/tealdeer/releases/latest/download/tealdeer-linux-x86_64-musl > "$HOME/bin/tldr"
-    chmod +x "$HOME/bin/tldr"
+    curl -LJ https://github.com/dbrgn/tealdeer/releases/latest/download/tealdeer-linux-x86_64-musl > "/usr/local/bin/tldr"
+    chmod +x "/usr/local/bin/tldr"
 fi
 if [ ! -d "$custom/plugins/tldr" ]; then
     mkdir "$custom/plugins/tldr"
