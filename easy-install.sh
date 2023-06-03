7e800f635db86d3677479203d19eae424fe4f340
Iliia <iliyamalecki@gmail.com>
2023-06-04 00:39:59 +0200

    refactored get-tldr

--- easy-install.sh
+++ easy-install.sh
@@ -7,34 +7,7 @@ yes | git clone https://github.com/esc/conda-zsh-completion $custom/plugins/cond
 yes | git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $custom/plugins/zsh-syntax-highlighting
 yes | git clone https://github.com/zsh-users/zsh-autosuggestions $custom/plugins/zsh-autosuggestions
 if ! command -v tldr >/dev/null; then
-    echo installing tldr
-    if [ "$EUID" -eq 0 ]; then
-        bindir="/usr/local/bin"
-    else
-        bindir="$HOME/bin"
-        mkdir $bindir
-    fi
-
-    declare -A arch_table=(
-        ["arm"]="tealdeer-linux-arm-musleabi"
-        ["armhf"]="tealdeer-linux-arm-musleabihf"
-        ["armv7"]="tealdeer-linux-armv7-musleabihf"
-        ["i686"]="tealdeer-linux-i686-musl"
-        ["x86_64"]="tealdeer-linux-x86_64-musl"
-    )
-
-    machine_arch=$(uname -m)
-
-    if [[ -n ${arch_table[$machine_arch]} ]]; then
-        chosen_file=${arch_table[$machine_arch]}
-    else
-        echo "Unsupported architecture: $machine_arch"
-    fi
-
-    echo "Chosen file: $chosen_file"
-
-    curl -LJ https://github.com/dbrgn/tealdeer/releases/latest/download/$chosen_file > "$bindir/tldr"
-    chmod +x "$bindir/tldr"
+    zsh get-tldr.zsh
 fi
 if [ ! -d "$custom/plugins/tldr" ]; then
     mkdir "$custom/plugins/tldr"
